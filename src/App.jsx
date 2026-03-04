933862c2c4a5ceb97de6569c5a5228a82e5fb12d
ilya3052 <ipopadin5@gmail.com>
2026-03-04 11:28:07 +0300

    основной файл приложения

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import LoginForm from './pages/login/Login.jsx';
 import ProfileSection from './pages/profile/ProfileSection.jsx';
 
@@ -6,32 +6,36 @@ import RegistrationForm from "./pages/registration/Registration.jsx";
 import ProtectedLayout from "./components/protectedLayout/protectedLayout.jsx";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import EmailActivation from "./pages/emailActivation/EmailActivation.jsx";
 
 const App = () => {
+    const location = useLocation();
+
+    const isAuthPage =
+        location.pathname === "/login" ||
+        location.pathname === "/registration";
     return (
         <div className="app">
 
-            {/* Public routes — без хедера и футера */}
-            <Routes>
-                <Route path="/login" element={<LoginForm />} />
-                <Route path="/registration" element={<RegistrationForm />} />
-            </Routes>
+            {!isAuthPage && <Header />}
 
-            {/* Protected routes — с хедером и футером */}
-            <div className="protected-wrapper">
-                <Header />
-                <main className="main">
-                    <Routes>
-                        <Route element={<ProtectedLayout />}>
-                            <Route path="/" element={<ProfileSection />} />
-                            <Route path="/profile" element={<ProfileSection />} />
-                        </Route>
-                    </Routes>
-                </main>
-                <Footer />
+            <main className="main">
+                <Routes>
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/registration" element={<RegistrationForm />} />
+
+                    {/* Protected */}
+                    <Route element={<ProtectedLayout />}>
+                        <Route path="/" element={<ProfileSection />} />
+                        <Route path="/profile" element={<ProfileSection />} />
+                        <Route path="/email/activate" element={<EmailActivation />} />
+
+                    </Route>
+                </Routes>
+            </main>
+
+            {!isAuthPage && <Footer />}
             </div>
-
-        </div>
     );
 }
 
