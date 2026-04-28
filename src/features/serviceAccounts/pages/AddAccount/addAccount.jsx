8566ed080f04b8d9087a56a6c1a225c1b4cd2e59
ilya3052 <ipopadin5@gmail.com>
2026-04-28 11:01:54 +0300

    создание страницы добавления сервисных аккаунтов

---
+++ src/features/serviceAccounts/pages/AddAccount/addAccount.jsx
@@ -0,0 +1,72 @@
+// AddAccountPage.jsx
+import React, {useEffect, useState} from 'react';
+import styles from './addAccount.module.css';
+import PlatformSelector from "../../../../components/platformSelector/platformSelector.jsx";
+import DataForm from "../../components/DataForm/dataForm.jsx";
+import {useNavigate} from "react-router-dom";
+import {API_VERSION, BASE_URL, verifyAndRefreshToken} from "../../../../utils/utils.js";
+
+
+const AddAccountPage = () => {
+    const [platforms, setPlatforms] = useState([]);
+
+    const [activePlatform, setActivePlatform] = useState(null);
+    const [loadingPlatforms, setLoadingPlatforms] = useState(true);
+
+    
+    
+    const navigate = useNavigate();
+
+    const fetchPlatforms = async () => {
+        try {
+            let token = localStorage.getItem("access_token");
+            if (!token) {
+                if (!(await verifyAndRefreshToken())) {
+                    navigate("/login");
+                    return;
+                }
+                return;
+            }
+            const res = await fetch(`${BASE_URL}/${API_VERSION}/social-entities/platforms/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setPlatforms(data);
+            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoadingPlatforms(false);
+        }
+    }
+
+
+    useEffect(() => {
+        fetchPlatforms();
+    }, [navigate])
+
+    const platform = platforms.find(p => p.alias === activePlatform);
+
+    return (
+        <main className={styles.addAccountContainer}>
+            <h1 className={styles.pageTitle}>Добавить сервисный аккаунт</h1>
+
+            <PlatformSelector
+                platforms={platforms}
+                activePlatform={activePlatform}
+                setActivePlatform={setActivePlatform}
+                loading={loadingPlatforms}
+            />
+
+            {/* Динамический блок ввода данных в зависимости от платформы */}
+            {activePlatform && <DataForm platform={platform}/>}
+
+        </main>
+    );
+};
+
+export default AddAccountPage;
