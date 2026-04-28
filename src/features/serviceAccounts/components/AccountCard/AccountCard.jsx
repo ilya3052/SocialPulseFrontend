efc2dab4e9895ab31ff7b2a0cfa4bed8a016d7de
ilya3052 <ipopadin5@gmail.com>
2026-04-28 11:02:58 +0300

    создание компонента карточки аккаунта

---
+++ src/features/serviceAccounts/components/AccountCard/AccountCard.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from './AccountCard.module.css';
+
+const AccountCard = ({account, onDelete, onActivate, platform, accountsCount}) => {
+    const getLoadColor = (percentage) => {
+        if (percentage <= 33) return styles.loadLow;
+        if (percentage <= 66) return styles.loadMedium;
+        return styles.loadHigh;
+    };
+
+
+    return (
+        <div className={styles.accountCard}>
+            <div className={styles.accountInfo}>
+                <span className={styles.accountName}>{account.name}</span>
+            </div>
+            {account.is_activated ? <>
+                <div className={styles.accountGroups}>{account.groups_count} групп</div>
+                <div className={styles.accountLoad}>
+
+                    <span className={`${styles.loadPercentage} ${getLoadColor(account.load)}`}>
+                        {account.load}%
+                    </span>
+                    <div className={styles.loadBar}>
+                        <div
+                            className={`${styles.loadFill} ${getLoadColor(account.load)}`}
+                            style={{width: `${account.load}%`}}
+                        ></div>
+                    </div>
+                </div>
+            </> : <span className={styles.notActivated}>не активирован</span>}
+
+            <div className={styles.actions}>
+                {account.is_activated ? (
+                    <span className={styles.statusActive}>
+                        <span>●</span> активен
+                    </span>
+                ) : (
+                    <button
+                        className={`${styles.actionLink} ${styles.activateLink}`}
+                        onClick={() => onActivate(account.id)}
+                    >
+                        активировать
+                    </button>
+                )}
+                {accountsCount > 1 && <button
+                    className={`${styles.actionLink} ${styles.deleteLink}`}
+                    onClick={() => onDelete(account.id, platform)}
+                >
+                    удалить
+                </button>}
+            </div>
+        </div>
+    );
+};
+
+export default AccountCard;
